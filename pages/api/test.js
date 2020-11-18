aaf8fc641710a6e1f6e5c126d521696b6327d75c
ojotuk <ojotuk14@yahoo.com>
2020-11-18 02:25:14 +0100

    fixed mongo

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,2 @@
-import { connectToDatabase } from "../../util/mongodb";
 
-export default async (req, res) => {
-  const { db } = await connectToDatabase();
-  const movies = await db.collection("testvaults").find({}).limit(20).toArray();
 
-  res.json(movies);
-};
