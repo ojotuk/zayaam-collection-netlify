7ebc5ccf5a6954a3119723e668fc56a760534d8c
ojotuk <ojotuk14@yahoo.com>
2020-11-18 02:07:27 +0100

    updated title

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
 import Head from "next/head";
-import Link from "next/link";
 import Header from "../components/Header";
 import Hero from "../components/Home/Hero";
-import productSample from "../components/Home/ProductSample";
 import Footer from "../components/Home/Footer";
 import FeaturedProduct from "../components/Home/FeaturedProduct";
 import Promotion from "../components/Home/Promotion";
 import styles from "../styles/Home.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMapPin,
-  faTachometerAlt,
-  faYinYang,
-} from "@fortawesome/free-solid-svg-icons";
-import { faYarn, faYCombinator } from "@fortawesome/free-brands-svg-icons";
 import ProductSample from "../components/Home/ProductSample";
 export default function Home({ isConnected }) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Zayaam Collections</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
@@ -40,7 +31,6 @@ export default function Home({ isConnected }) {
 }
 
 // export async function getServerSideProps(context) {
- 
 
 //   return {
 //     props: { isConnected },
